81e760796186ce6ebb0746f9813d6ab401b75f7d
zplat <zplat@conversantmedia.com>
2019-05-06 14:15:59 -0700

    added emoji to dirty git

--- prompt.sh
+++ prompt.sh
@@ -14,7 +14,7 @@ find_git_branch() {
 find_git_dirty() {
   local status=$(git status --porcelain 2> /dev/null)
   if [[ "$status" != "" ]]; then
-    git_dirty='*'
+    git_dirty='💾'
   else
     git_dirty=''
   fi
