be13eb6dc3c7c8ae38f3394773b110249ab806b5
Zachary Plat <zshino20z@gmail.com>
2019-05-15 21:24:59 -0700

    removed emoji for linux terminal

--- prompt.sh
+++ prompt.sh
@@ -14,7 +14,7 @@ find_git_branch() {
 find_git_dirty() {
   local status=$(git status --porcelain 2> /dev/null)
   if [[ "$status" != "" ]]; then
-    git_dirty='💾'
+    git_dirty='*'
   else
     git_dirty=''
   fi
